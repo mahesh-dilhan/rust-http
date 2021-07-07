014127d372ca0f2209d3845f89bf84c58687d02f
mahesh <mail.mahesh.lk@gmail.com>
2021-07-07 19:57:12 +0800

    Rust http

---
+++ rusthttp/target/debug/deps/httpserver.d
@@ -0,0 +1,5 @@
+/Users/mahesh/play/rust/rusthttp/target/debug/deps/httpserver: src/main.rs
+
+/Users/mahesh/play/rust/rusthttp/target/debug/deps/httpserver.d: src/main.rs
+
+src/main.rs:
---
+++ rusthttp/target/debug/httpserver.d
@@ -0,0 +1,1 @@
+/Users/mahesh/play/rusthttp/rusthttp/target/debug/httpserver: /Users/mahesh/play/rusthttp/rusthttp/src/main.rs
